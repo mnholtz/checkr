34493eee5be880deff31c49fa0fa731ce12c211f
mnholtz <mnholtz@ucsc.edu>
2018-06-10 20:39:57 -0700

    fix heatmap and vue compatibility issue

---
+++ static/js/heatmap.js
@@ -0,0 +1,27 @@
+var cal = new CalHeatMap();
+cal.init({
+        itemSelector: "#heatmap",
+        domain: "month",
+        subDomain: "x_day",
+        cellSize: 30, legendCellSize: 15,
+        cellPadding: 3, legendCellPadding: 3,
+        domainGutter: 10,
+        subDomainTextFormat: "%d",
+        domainDynamicDimension: false,
+        verticalOrientation: true,
+        tooltip: true,
+        legendVerticalPosition: "left",
+      	nextSelector: "#cal-heatmap-next",
+      	previousSelector: "#cal-heatmap-previous",
+      	itemNamespace: "domainDynamicDimension",
+        label: {
+                 position: "left"
+            },
+        onClick: function(date, nb) {
+          console.log("you clicked on " + date);
+      		$("#cal-heatmap").html("You just clicked <br/>on <b>" +
+      			date + "</b> <br/>with <b>" +
+      			(nb === null ? "unknown" : nb) + "</b> items"
+      		);
+      	}
+});
