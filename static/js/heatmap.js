3641a8c24061d1549b3818a95987be70b4c5b11d
mnholtz <mnholtz@ucsc.edu>
2018-06-11 10:13:06 -0700

    improve heatmap styling

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,13 +1,25 @@
 var cal = new CalHeatMap();
 cal.init({
         itemSelector: "#cal-heatmap",
+        data: [	{date: 1528691203, value: 0},
+				{date: 1527913602, value: 1},
+				{date: 1528000002, value: 0}],
+		range: 2, legend: [0, 1],
+			legendColors: {
+			min: "#efefef",
+			max: "steelblue",
+			empty: "white"
+			// Will use the CSS for the missing keys
+		},
+        dataType: "json",
+        range: 25,
         domain: "month",
         subDomain: "x_day",
         cellSize: 30, legendCellSize: 15,
         cellPadding: 3, legendCellPadding: 3,
         domainGutter: 10,
         subDomainTextFormat: "%d",
-        displayLegend: false,
+        displayLegend: true,
         domainDynamicDimension: false,
         verticalOrientation: true,
         legendVerticalPosition: "top",
@@ -15,13 +27,10 @@ cal.init({
       	previousSelector: "#cal-heatmap-previous",
       	itemNamespace: "domainDynamicDimension",
         label: {
-                 position: "left"
+                 position: "left",
+                 rotation: "left"
             },
         onClick: function(date, nb) {
           console.log("you clicked on " + date);
-      		$("#cal-heatmap").html("You just clicked <br/>on <b>" +
-      			date + "</b> <br/>with <b>" +
-      			(nb === null ? "unknown" : nb) + "</b> items"
-      		);
       	}
 });
